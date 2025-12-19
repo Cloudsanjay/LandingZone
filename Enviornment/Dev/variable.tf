e9dc4e521eb21700c4139d52216aa44490382fe1
Telecom <SanjaySingh@chandankumar02731gmail.onmicrosoft.com>
2025-12-19 15:47:34 +0530

    variable type added

--- Enviornment/Dev/variable.tf
+++ Enviornment/Dev/variable.tf
@@ -1,18 +1,80 @@
-variable "rgs" {} 
-variable "publicips" {} 
-variable "networking" {} 
-variable "virtual_machine" {}
-variable "key_vault" {}
-variable "vault_secret" {}
+variable "rgs" {
+    type = map(object({
+    name = string
+    location = string
+    managed_by = optional(string)
+    tags = optional(map(string))
+  }))
 
-# variable "key_vault" {
-#   type = map(object({
-#     name                        = string
-#     location                    = string
-#     resource_group_name         = string
-#     enabled_for_disk_encryption = bool
-#     soft_delete_retention_days  = number
-#     purge_protection_enabled    = bool
-#     sku_name                    = string
-#   }))
-# }
+} 
+variable "publicips" {
+    type = map(object({
+    name                = string
+    resource_group_name = string
+    location            = string
+    allocation_method   = string
+    sku                 = optional(string)
+    sku_tier            = optional(string)
+  }))
+} 
+
+variable "networking" {
+    type = map(object({
+    name                = string
+    location            = string
+    resource_group_name = string
+    address_space       = list(string)
+    dns_servers         = optional(list(string))
+    tags                = optional(map(string))
+    subnets = optional(list(object({
+      name             = string
+      address_prefixes = list(string)
+    })), [])
+  }))
+} 
+
+variable "virtual_machine" {
+    type = map(object({
+    subnets                       = string
+    virtual_network_name          = string
+    resource_group_name           = string
+    pipname                       = string
+    nic                           = string
+    location                      = string
+    ipname                        = string
+    private_ip_address_allocation = string
+    vmname                        = string
+    size                          = string
+    caching                       = string
+    storage_account_type          = string
+    publisher                     = string
+    offer                         = string
+    sku                           = string
+    version                       = string
+    keyvaultname                  = optional(string)
+    username                      = optional(string)
+    userpassword                  = optional(string)
+  }))
+}
+
+variable "key_vault" {
+    type = map(object({
+    name                        = string
+    location                    = string
+    resource_group_name         = string
+    enabled_for_disk_encryption = bool
+    soft_delete_retention_days  = number
+    purge_protection_enabled    = bool
+    sku_name                    = string
+  }))
+}
+
+variable "vault_secret" {
+    type = map(object({
+      keyvaultname = string
+      resource_group_name = string
+      name = string
+      value = string
+    }))
+}
+
